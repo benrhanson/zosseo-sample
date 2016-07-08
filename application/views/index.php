dfaecfac96094a57a18de7070c6d30797f2a1f3f
benrhanson <benrhanson@Benjamins-MacBook-Pro.local>
2016-07-08 11:51:40 -0700

    first commit

---
+++ application/views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style.css'); ?>" />
+</head>
+<body>
+	<div id = 'upper'>
+		<h1>CodeIgniter Launch Pad</h1>
+	</div>
+</body>
+</html>

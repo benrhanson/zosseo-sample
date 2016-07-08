5a78d012bf964eb7c90992b482779356d71e8879
Owen Lowry <lowryos@yahoo.com>
2016-07-08 12:54:53 -0700

    Created a header

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Pages extends CI_Controller {
 
 	public function __construct()
 	{
@@ -25,7 +25,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('header');
+		$this->load->view('home');
 	}
 }
 

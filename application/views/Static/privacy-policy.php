edef5bba6b22c9726a0c4db5b4d0b217fcf3c2c0
Ben Hanson <Ben Hanson>
2016-07-09 21:55:48 -0700

    fixed privacy policy and refunds pages

--- application/views/Static/privacy-policy.php
+++ application/views/Static/privacy-policy.php
@@ -1,12 +1,33 @@
 <div class = "content">
-	<h1>I cam the content div</h1>
+	<h2 class = "refund-title">ZOSSEO<sup>TM</sup> Privacy Policy</h2>
 
-	<div class = "left col-md-5">
-		<p>I AM THE LEFT COLUMN.</p>
-	</div>
+	<h3>What information do we collect?</h3>
+	<p>We collect information from you when you place an order. When ordering or registering on our site, as appropriate, you may be asked to enter your: name, e-mail address, mailing address, phone number or credit card information. You may, however, visit our site anonymously.</p> 
+	<h3>What do we use your information for?</h3>
+	<p>Any of the information we collect from you may be used in one of the following ways: </p>
+	<ul class = "privacy-list">
+		<li>To process transactions Your information, whether public or private, will not be sold, exchanged, transferred, or given to any other company for any reason whatsoever, without your consent, other than for the express purpose of delivering the purchased product or service requested.</li>
+		<li>To send periodic emails The email address you provide for order processing, will only be used to send you information and updates pertaining to your order. If you decide to opt-in to our mailing list, you will receive emails that may include company news, updates, related product or service information, etc. Note: If at any time you would like to unsubscribe from receiving future emails, we include detailed unsubscribe instructions at the bottom of each email. </li>
+	</ul> 
+	<h3>How do we protect your information?</h3> 
+	<p>We implement a variety of security measures to maintain the safety of your personal information when you place an order We offer the use of a secure server. All supplied sensitive/credit information is transmitted via Secure Socket Layer (SSL) technology and then encrypted into our Payment gateway providers database only to be accessible by those authorized with special access rights to such systems, and are required to keep the information confidential. After a transaction, your private information (credit cards, social security numbers, financials, etc.) will not be stored on our servers.</p>
+	<h3>Do we use cookies?</h3>
+	<p>We do not use cookies.</p>
+	<h3>Do we disclose any information to outside parties?</h3>
+	<p>We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.</p>
+	<h3>California Online Privacy Protection Act Compliance</h3> 
+	<p>Because we value your privacy we have taken the necessary precautions to be in compliance with the California Online Privacy Protection Act. We therefore will not distribute your personal information to outside parties without your consent.</p>
+	<h3>Childrens Online Privacy Protection Act Compliance</h3>
+	<p>We are in compliance with the requirements of COPPA (Childrens Online Privacy Protection Act), we do not collect any information from anyone under 13 years of age. Our website, products and services are all directed to people who are at least 13 years old or older. </p>
+	<h3>Online Privacy Policy Only</h3>
+	<p>This online privacy policy applies only to information collected through our website and not to information collected offline.</p> 
+	<h3>Your Consent</h3>
+	<p>By using our site, you consent to our privacy policy. Changes to our Privacy Policy If we decide to change our privacy policy, we will post those changes on this page, send an email notifying you of any changes, and/or update the Privacy Policy modification date below. This policy was last modified on March 1, 2010.</p>
+	<h3>Contacting Us</h3>
+	<h4>ZOSSEO Implant Drill Stops </h4>
+	<h4>509 Olive Way ste 1711</h4>
+	<h4>Seattle, WA 98101</h4> 
 
-	<div class = "right col-md-7">
-		<p>I AM THE RIGHT COLUMN.</p>
-	</div>
+	<p>For further questions, please call (206) 453-5963 or email info@zosseo.com</p>
 
 </div> <!--content ends -->
--- application/views/Static/refund.php
+++ application/views/Static/refund.php
@@ -1,12 +1,13 @@
 <div class = "content">
-	<h1>I cam the content div</h1>
+	<h2 class = "refund-title">ZOSSEO<sup>TM</sup> Refund Policy</h2>
+	
+	<p>If for any reason you are not satisfied with a product purchased from www.zosseo.com you can return it within 30 days of the original purchase date. All returns must include product invoice and all of the product components for a full refund. Returned packages with missing drill stops, grommets, or metal inserts will not be refunded. All credit card purchases will be refunded back to the credit card that the product was purchased on. Shipping and handling fees are non-refundable.</p>
 
-	<div class = "left col-md-5">
-		<p>I AM THE LEFT COLUMN.</p>
-	</div>
+	<h3>Send returns to: </h3>
 
-	<div class = "right col-md-7">
-		<p>I AM THE RIGHT COLUMN.</p>
-	</div>
+	<h4>ZOSSEO Implant Drill Stops </h4>
+	<h4>509 Olive Way ste 1711</h4>
+	<h4>Seattle, WA 98101</h4> 
 
+	<p>For further questions, please call (206) 453-5963 or email info@zosseo.com</p>
 </div> <!--content ends -->

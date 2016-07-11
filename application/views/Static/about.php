9fc7ce66e9988a767428ff80e4a2567080c3efcb
Owen Lowry <lowryos@yahoo.com>
2016-07-11 13:00:11 -0700

    Finishing touches to about page, carousel

--- application/views/Static/about.php
+++ application/views/Static/about.php
@@ -1,12 +1,35 @@
 <div class = "content">
 
-	<div class = "left col-md-5">
-		<img id="first-about-pic" src="assets/images/about/about-left-1.jpg">
-		<img src="assets/images/about/about-left-2.jpg">
+	<div class="video">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/02Ip6PFuDJE" frameborder="0" allowfullscreen></iframe>
 	</div>
 
-	<div class = "right col-md-7 about-content">
-		<a href="https://www.youtube.com/watch?v=02Ip6PFuDJE">Watch this to see how ZOSSEO<sup>TM</sup> works!</a>
+	<div class="about-content">
+		<h3 class="orangeyy">Simple Design Reduces Time and Expense</h3>
+	</div>
+	<div>
+		<ul class="privacy-list-1">
+			<li>Single width for ease-of-use, and precise, consistent performance</li>
+			<li>25 differing stock lengths available</li>
+			<li>Custom lengths and kits available</li>
+			<li>Rugged stainless steel case makes organization and selection convenient</li>
+			<li>Reusable – the entire set is both cold and autoclave sterilizable to 330 degrees</li>
+			<li>Can be easily modified to custom lengths</li>
+		</ul>
 	</div>
 
+	<div class="about-image-1">
+		<img src="assets/images/about/about-right-1.jpg">
+	</div>
+
+	<div class="about-content">
+		<h3 class="orangeyy drillstop-title">Drillstops Used Through Surgical Guides</h3>
+		<img src="assets/images/about/about-right-2.png" alt="Drillstop Guide">
+		<p class="drillstops-text">Most guide manufacturers do an excellent job of determining ideal implant position with a cbct image and provide stents to the clinician for the implant placement. The one variable that remains unmanaged is the depth of the drill penetration. Laboratories recommend clinicians use single use drills and multiple stents with stops built in (added cost) or suggest using surgical guide inserts from the drill manufacturers with drills of varying width and length with built in stops (added cost). Using inexpensive, reusable drill stops in a guide with a matching tube diameter replaces all of these options with one simpler, and more economic.</p>
+	</div>
+
+	<div class="trademark about-content">
+		<p>The ZOSSEO<sup>TM</sup> trademark is owned by Joseph R. Zimmer DDS P.C.</p>
+		<p class="small-text">The ZOSSEO<sup>TM</sup> Universal Implant Drill Stops utilize patent pending technology to prevent over-drilling, reducing the potential for sinus or nerve complications during osteotomies.
+	</div>
 </div> <!--content ends -->
